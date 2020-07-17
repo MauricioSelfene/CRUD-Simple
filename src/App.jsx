a57f4dd860d114f9fe846b5373c0c582bccae6fd
Mauricio Selfene Fredes <m.selfene@gmail.com>
2020-07-16 22:41:05 -0400

    primer commit para realizar proyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="text-center">CRUD Simple</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
